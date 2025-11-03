b6c3eda35518fe9b7249b2e5c6eac7cc508a1a27
easysajusaju-dev <easysaju.saju@gmail.com>
2025-11-03 18:50:57 +0900

    Update form.js

--- form.js
+++ form.js
@@ -130,7 +130,8 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
       // ✅ 여기서 thankyou.html로 가던 것을 payment.html로 변경 (기존 기능 외 변동 없음)
       const payUrl = `payment.html?oid=${encodeURIComponent(orderId)}&product=${encodeURIComponent(productName)}&price=${productPrice}`;
-      window.location.href = payUrl;
+      window.location.href = thankyouUrl;
+
 
     }catch(err){ 
       console.error(err); 
